b05deace170ac50bb2f74c8455ac361761c33fc1
DaryaLunik <dawa23112007@gmail.com>
2024-12-17 11:19:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_17._1
+{
+    class Program
+    {
+        /// <summary>
+        /// Задание 2. Записана информация о стоимости каждого из 20 видов товара, продаваемых фирмой. С 1
+        /// января очередного года фирма прекращает продавать товар, стоимость которого записана в n-м элементе
+        /// списка. Получить список со стоимостью всех оставшихся видов товара.
+        /// </summary>
+        static void Output(List<string> car)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
+
+
+
+        }
+    }
+}
